4d90468baf44a665733b43a05adba87439e2720a
jailennn <111888334+jailennn@users.noreply.github.com>
2024-08-29 21:31:25 -0400

    Update testing.sh

--- School Projects/testing.sh
+++ School Projects/testing.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-# Source the dice_roller.sh file to use the roll_dice function
+# source the dice_roller.sh file to use the roll_dice function
 source ./dice_roller.sh
 
-# Function to run trials and tally results
+# function to run trials and tally results
 run_trials() {
     local trials=$1
-    declare -A tally
+    declare -A tally # associative array declaration
 
     for ((i=0; i<$trials; i++)); do
-        # Roll 5 dice without outputting the result
+        # roll 5 dice and store them in a variable
         rolled_numbers=$(roll_dice 5)
 
-        # Tally the results
+        # tally the results in the associative array
         for num in $rolled_numbers; do
             ((tally[$num]++))
         done
     done
 
-    # Echo the tally results
+    # display the tally results
     echo "Tally of rolled numbers after $trials trials:"
     for num in "${!tally[@]}"; do
         echo "$num: ${tally[$num]}"
     done
 }
 
-# Number of trials to run
+# specify Number of trials to run
 num_trials=60 
 
 # Run the trials
+echo "Starting Test 1..."
 run_trials $num_trials

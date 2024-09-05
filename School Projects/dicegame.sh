858d14686fd63a286339a092d5e9a8af1becbc79
jailennn <111888334+jailennn@users.noreply.github.com>
2024-09-05 13:33:45 -0400

    Update dicegame.sh

--- School Projects/dicegame.sh
+++ School Projects/dicegame.sh
@@ -204,7 +204,8 @@ run_trials() {
     fi
 
     # Output the total number of lagged correlations (repeats)
-    echo "Repeats - $correlation_count"
+    repeat_percentage=$(echo "scale=2; $repeat_count * 100 / $trials" | bc -l)
+    echo "Repeats - $correlation_count, $repeat_percentage%"
 }
 # call to start playing the game
 start_game
